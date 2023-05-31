a6fd19a69c4409e559e9d59d571753f50d25935b
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 <dohyun911@hanmail.net>
2023-05-31 12:32:04 +0900

    로딩바 및 숫자 퍼센트  추가 수정 최종

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
+const mask = document.querySelector('aside');
+const delay = convertSpeed(mask);
 const target = 'figure';
 const num = 200;
-const mask = document.querySelector('aside');
-const convertedSpeed = convertSpeed(mask);
 
 //figure에 안쪽에 동적으로 200개 이미지 돔생성해서 리턴하는 함수 호출
 //동적으로 돔을 생성하자마자 리턴해주는 요소를 바로 변수에 담음
@@ -22,7 +22,7 @@ imgDOM.forEach((img) => {
 
 			setTimeout(() => {
 				mask.remove();
-			}, convertedSpeed);
+			}, delay);
 		}
 	};
 	img.onerror = () => {
@@ -30,7 +30,7 @@ imgDOM.forEach((img) => {
 	};
 });
 
-//마우스무스시 포인터 좌표값 200분율로 변경
+//마우스무브시 포인터 좌표값 200분율로 변경
 window.addEventListener('mousemove', (e) => matchMove(imgDOM, num, e));
 
 //동적 이미지 생성 함수
@@ -41,16 +41,13 @@ function createImgs(target, num) {
 		imgs += `<img src='img/pic${i}.jpg' />`;
 	}
 	frame.innerHTML = imgs;
-	//돔을 생성하자마자 바로 활용할 수 있도록 생성된 요소를 내보내주기까지하는 코드
+	//DOM생성하자마자 바로 활용할수있도록 생성된 요소를 내보내주는 코드
 	return frame.querySelectorAll('img');
 }
-
-//돔이 생성되었다고 로딩이 완료된건 아님 (틀만 완성됨)
 
 //마우스 포인터 위치와 이미지 순서 매칭 함수
 function matchMove(arrEl, num, e) {
 	const percent = parseInt((e.clientX / window.innerWidth) * num);
-
 	for (const img of arrEl) img.style.display = 'none';
 	arrEl[percent].style.display = 'block';
 }
